ae0fe7719243e7e02ea4425f194eca9356cf8b6f
Àngel Ollé Blázquez <angel@olleb.com>
2022-12-12 14:02:33 +0100

    Add last names and expose OCP route

--- user-storage-spi/jaxrs-user-store/src/main/java/com/olleb/service/UserService.java
+++ user-storage-spi/jaxrs-user-store/src/main/java/com/olleb/service/UserService.java
@@ -13,34 +13,40 @@ import com.olleb.model.User.UserBuilder;
 @ApplicationScoped
 public class UserService {
 
-    private final Map<String, User> userMap = Map.of(
+    private static final Map<String, User> userMap = Map.of(
             "1", new UserBuilder("1", "nata")
                     .setFirstName("Nata")
+                    .setLastName("Natilla")
                     .setEmail("nata@example.com")
                     .setPassword("natapassword")
                     .build(),
             "2", new UserBuilder("2", "freud")
                     .setFirstName("Freud")
+                    .setLastName("Redhead")
                     .setEmail("freud@example.com")
                     .setPassword("freudpassword")
                     .build(),
             "3", new UserBuilder("3", "mao")
                     .setFirstName("Mao")
+                    .setLastName("Meow")
                     .setEmail("mao@example.com")
                     .setPassword("maopassword")
                     .build(),
             "4", new UserBuilder("4", "dark")
                     .setFirstName("Dark")
+                    .setLastName("Butcher")
                     .setEmail("dark@example.com")
                     .setPassword("darkpassword")
                     .build(),
             "5", new UserBuilder("5", "abel")
                     .setFirstName("Abel")
+                    .setLastName("Miiii")
                     .setEmail("abel@example.com")
                     .setPassword("abelpassword")
                     .build(),
             "6", new UserBuilder("6", "flash")
                     .setFirstName("Flash")
+                    .setLastName("Jr")
                     .setEmail("flash@example.com")
                     .setPassword("flashpassword")
                     .build()
