45b9ce8f7f3057c113c9f571bbc2479d93d71c2d
Àngel Ollé Blázquez <angel@olleb.com>
2023-01-23 12:57:31 +0100

    fix scripts

--- vault-spi/conjur/start.sh
+++ vault-spi/conjur/start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+#set -x
 
 FILE=$(mktemp)
 trap 'rm -rf -- "$FILE"' EXIT
 
-make all
+#make all
 
 podman exec -ti conjur conjurctl account create demoAccount > $FILE
 
-APIKEY=$(sed -n '/API/ s/.*: *//p' $FILE)
+export APIKEY=$(sed -n '/API/ s/.*: *//p' $FILE | tr -d '\r')
+echo "APIKEY: $APIKEY\n"
 
 # TODO refactor
-TOKEN=$(curl --header "Accept-Encoding: base64" -d $APIKEY http://localhost:18080/authn/demoAccount/admin/authenticate)
+echo "creating the conjur stuff..."
+
+TOKEN=$(curl --header "Accept-Encoding: base64" -d "${APIKEY}" http://localhost:18080/authn/demoAccount/admin/authenticate)
+#echo "token: $TOKEN"
+
 curl -H "Authorization: Token token=\"${TOKEN}\"" -d "$(< policy.yml)" http://localhost:18080/policies/demoAccount/policy/root
 curl -H "Authorization: Token token=\"${TOKEN}\"" -d "password" http://localhost:18080/secrets/demoAccount/variable/ldap/password
 #curl -k -H "Authorization: Token token=\"${TOKEN}\"" http://localhost:18080/secrets/demoAccount/variable/ldap/password
